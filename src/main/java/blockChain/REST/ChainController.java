ef48c1bd2a5695d41a8dab3249801be6f7c8f8d4
Chris <ccjacobs94@gmail.com>
2018-09-01 18:30:39 -0500

    Added Swagger configuration

--- src/main/java/blockChain/REST/ChainController.java
+++ src/main/java/blockChain/REST/ChainController.java
@@ -1,16 +1,24 @@
 package blockChain.REST;
 
-import org.springframework.web.bind.annotation.RestController;
+import blockChain.Containers.Block;
+import blockChain.Containers.BlockChain;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ChainController {
+
+    private BlockChain blockChain = new BlockChain();
 
     /**
      * adds a transaction to the underlying BlockChain
      * @param transaction The transaction to be added
      */
-    public void addTransaction(String[] transaction){
-
+    @RequestMapping(value = "/addTransaction", method = RequestMethod.POST)
+    public List<Block> addTransaction(@RequestBody() String[] transaction){
+        blockChain.addTransaction(transaction);
+        return blockChain.getBlockChain();
     }
 
 }
--- src/main/java/blockChain/REST/DemoApplication.java
+++ src/main/java/blockChain/REST/DemoApplication.java
@@ -1,4 +1,4 @@
-package blockChain.main;
+package blockChain.REST;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
