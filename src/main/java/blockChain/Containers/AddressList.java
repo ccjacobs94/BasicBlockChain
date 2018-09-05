5315b18fb81bd5668cb1772f53da6607a490a28c
Chris <ccjacobs94@gmail.com>
2018-09-04 22:07:05 -0500

    Removed unused imports

--- src/main/java/blockChain/Containers/AddressList.java
+++ src/main/java/blockChain/Containers/AddressList.java
@@ -1,8 +1,5 @@
 package blockChain.Containers;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
